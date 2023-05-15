7091a38c2f1950e71f2c8a8f465b36a7354d5954
GabrielMartinelli <gabrielmartinelli10@gmail.com>
2023-05-15 12:26:43 -0300

    Project started

---
+++ src/routes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function AppRoutes() {
+  <BrowserRouter>
+    <Routes>
+      <Route></Route>
+    </Routes>
+  </BrowserRouter>;
+}
