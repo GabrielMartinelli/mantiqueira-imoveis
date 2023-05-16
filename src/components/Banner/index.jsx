c2b1730e2122e1ae38b90441d360e07e7fb569c7
GabrielMartinelli <gabrielmartinelli10@gmail.com>
2023-05-16 16:26:28 -0300

    Header alteration

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,6 +5,7 @@ import { styled } from "styled-components";
 const Img = styled.img`
   opacity: 0.7;
   width: 100%;
+  height: 100vh;
 `;
 
 export default function Banner() {
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,7 @@ const Nav = styled.nav`
   align-items: center;
   justify-content: space-between;
   background-color: var(--dark-blue);
-  opacity: 0.9;
+  opacity: 0.8;
   width: 100%;
   position: fixed;
   z-index: 100;
@@ -19,13 +19,9 @@ const ImgLogo = styled.img`
 `;
 
 const ImgIcon = styled.img`
-  margin-right: 2rem;
+  width: 24px;
+  margin-left: 1rem;
   cursor: pointer;
-
-  &:hover {
-    width: 33px;
-    transition: 0.9s linear;
-  }
 `;
 
 const NavList = styled.ul`
@@ -34,17 +30,44 @@ const NavList = styled.ul`
   color: var(--white);
 `;
 
+const NavListContact = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--white);
+  margin-right: 2rem;
+`;
+
 const List = styled.li`
   margin: 0 0.5rem;
   cursor: pointer;
-  font-weight: 700;
   font-size: 18px;
   padding: 8px;
 
   &:hover {
-    border-bottom: 2px solid var(--white);
+    border-bottom: 3px solid var(--white);
     line-height: 1.5;
   }
+`;
+
+const LinkContact = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.4rem;
+  width: 120px;
+  border-radius: 25px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--white);
+    color: var(--dark-blue);
+  }
+`;
+
+const TextSpan = styled.span`
+  font-size: 18px;
+  font-weight: 500;
 `;
 
 export default function Header() {
@@ -58,7 +81,38 @@ export default function Header() {
           <List>Quem somos</List>
           <List>Contato</List>
         </NavList>
-        <ImgIcon src={whatsapp} />
+        <NavListContact>
+          <li>
+            <LinkContact
+              href="https://api.whatsapp.com/send?phone=5535998234850&text=Olá Fausto, estou a procura de um imóvel, poderia me ajudar?"
+              target="_blank"
+              rel="noopener"
+            >
+              <TextSpan>Fausto</TextSpan>
+              <ImgIcon src={whatsapp} />
+            </LinkContact>
+          </li>
+          <li>
+            <LinkContact
+              href="https://api.whatsapp.com/send?phone=5535991509040&text=Olá Rafael, estou a procura de um imóvel, poderia me ajudar?"
+              target="_blank"
+              rel="noopener"
+            >
+              <TextSpan>Rafael</TextSpan>
+              <ImgIcon src={whatsapp} />
+            </LinkContact>
+          </li>
+          <li>
+            <LinkContact
+              href="https://api.whatsapp.com/send?phone=5535998985310&text=Olá Tiago, estou a procura de um imóvel, poderia me ajudar?"
+              target="_blank"
+              rel="noopener"
+            >
+              <TextSpan>Tiago</TextSpan>
+              <ImgIcon src={whatsapp} />
+            </LinkContact>
+          </li>
+        </NavListContact>
       </Nav>
     </header>
   );
