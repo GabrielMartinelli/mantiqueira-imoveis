2ac2b392f68bc2354989608fd4279caff782db57
GabrielMartinelli <gabrielmartinelli10@gmail.com>
2023-05-26 19:35:44 -0300

    Altered img Banner for past public

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { styled } from "styled-components";
+import banner from "/img/background-banner.jpg";
 
 const Div = styled.div`
   margin: 0;
   padding-top: var(--padding-top-header);
   width: 100%;
   height: 100%;
-  background: url("../src/components/Banner/background-banner.jpg") no-repeat center / cover;
+  background: url(${banner}) no-repeat center / cover;
 
   h1 {
     color: transparent;
